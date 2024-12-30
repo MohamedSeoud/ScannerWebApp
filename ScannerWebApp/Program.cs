f573c0d05bcc5603e5ae552aee925a2c01cb7b2a
Mohammed Seoud <m.seoud42@gmail.com>
2024-12-31 00:19:41 +0200

    commit

--- ScannerWebApp/Program.cs
+++ ScannerWebApp/Program.cs
@@ -2,6 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll", builder => {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 
 var app = builder.Build();
 
